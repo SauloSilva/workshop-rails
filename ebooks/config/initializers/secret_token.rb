c7fe56377af5a0e03ce417dfb2f5ae66e5b95880
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2013-09-06 00:41:44 -0300

    inciando o projeto

---
+++ ebooks/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ebooks::Application.config.secret_key_base = '541b66a911d4bf8c02a9f695fdb944f8bc31a8bf59c25d2c5eef6ba27c677729e3bb68f996fb38cfe7cf713532bccf30a3f102cef1255587f64691ba678be320'
