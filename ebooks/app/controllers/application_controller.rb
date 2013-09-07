046cfc6b4c4dd5321d5150caa4f08b74c5984219
saulo da silva santiago <saulodasilvasantiago@gmail.com>
2013-09-07 16:18:19 -0300

    finalizando o crud do usuario

--- ebooks/app/controllers/application_controller.rb
+++ ebooks/app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << [:name, :birthdate]
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:name, :email) }
+    devise_parameter_sanitizer.for(:account_update) << [:name, :birthdate]
   end
 end
